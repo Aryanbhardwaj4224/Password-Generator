00ec40b3957291ec236df5aed3bee64b1d756190
Aryan Bhardwaj <aryanbhardwaj4224@gmail.com>
2025-09-11 23:40:14 +0530

    some js added

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ let inputSlider = document.querySelector('.inputSlider');
 
 let sliderValue = document.querySelector('.sliderValue');
 
+//show input slider value 
+
 sliderValue.textContent=inputSlider.value;
 
 inputSlider.addEventListener('input',()=>{
     sliderValue.textContent=inputSlider.value;
 })
+
